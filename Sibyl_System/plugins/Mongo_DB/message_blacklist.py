4827e2a91a2a8249a4c197c29d74db1bd776b6fe
DragSama <49992845+DragSama@users.noreply.github.com>
2020-04-23 10:09:20 +0530

    clean up

--- Sibyl_System/plugins/Mongo_DB/message_blacklist.py
+++ Sibyl_System/plugins/Mongo_DB/message_blacklist.py
@@ -1,35 +1,31 @@
-import pymongo 
+import pymongo
 from Sibyl_System import MONGO_CLIENT
-import asyncio 
-import re 
-
+import asyncio
 
 
 async def get_blacklist():
-     db = MONGO_CLIENT['Sibyl']['Main']
-     #cant find better names
-     upd = {} 
-     owo = {}
-     json = db.find_one({'_id': 1})
-     return json.get('blacklisted', []) 
+    db = MONGO_CLIENT['Sibyl']['Main']
+    json = db.find_one({'_id': 1})
+    return json.get('blacklisted', [])
 
-async def update_blacklist(word, add = False):
-     db = MONGO_CLIENT['Sibyl']['Main']
-     #cant find better names
-     upd = {} 
-     owo = {}
-     bl = db.find_one({'_id': 1})
-     current = bl['blacklisted']
-     if add:
-        if word in current: 
-             return False
-        current.append(word)
-     else:
+
+async def update_blacklist(word, add=False):
+    db = MONGO_CLIENT['Sibyl']['Main']
+    # cant find better names
+    upd = {}
+    owo = {}
+    bl = db.find_one({'_id': 1})
+    current = bl['blacklisted']
+    if add:
         if word in current:
-          current.remove(word)
-        else: 
-          return False
-     upd['blacklisted'] = current
-     owo['$set'] = upd
-     db.update_one(db.find_one({'_id': 1}), owo)
-     return True
+            return False
+        current.append(word)
+    else:
+        if word in current:
+            current.remove(word)
+        else:
+            return False
+    upd['blacklisted'] = current
+    owo['$set'] = upd
+    db.update_one(db.find_one({'_id': 1}), owo)
+    return True
