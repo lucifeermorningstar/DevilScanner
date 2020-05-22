2e06acf904a28a8f61c1641c94af473e110add42
DragSama <49992845+DragSama@users.noreply.github.com>
2020-05-22 15:10:19 +0530

    changed image position

--- Sibyl_System/plugins/id_maker.py
+++ Sibyl_System/plugins/id_maker.py
@@ -5,15 +5,21 @@ import os
 @System.on(system_cmd(pattern = r'get_id'))
 async def image_maker(event) -> None:
  replied_user = await event.get_reply_message()
+ #Download profile photo
  await System.download_profile_photo(replied_user.from_id, file= 'user.png', download_big = True)
  user_photo = Image.open('user.png')
+ #open id photo
  id_template = Image.open('ID.png')
- user_photo = user_photo.resize((123, 134))
- id_template.paste(user_photo, (146, 66))
- position = (300, 56)
+ #resize user photo to fit box in id template
+ user_photo = user_photo.resize((989, 1073))
+ #put image in position
+ id_template.paste(user_photo, (1229, 573))
+ #postion on where to draw text
+ position = (2473, 481)
  draw = ImageDraw.Draw(id_template)
- color = 'rgb(23, 43, 226)' #blue color
+ color = 'rgb(23, 43, 226)' #blue-ish color
  font = ImageFont.truetype('Sibyl_System/plugins/arial-unicode-ms.ttf', size=30)
+ #put text in image
  draw.text(position, replied_user.sender.first_name, fill=color, font=font)
  id_template.save('user_id.png')
  await System.send_message(
@@ -24,5 +30,3 @@ async def image_maker(event) -> None:
         force_document=False,
         silent=True
     )
- 
- 
