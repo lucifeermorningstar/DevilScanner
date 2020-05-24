e37787e2ec0c5e9a2b587b75152fd4f17b3a1647
DragSama <49992845+DragSama@users.noreply.github.com>
2020-05-24 16:21:38 +0530

    Added shell in to_load

--- Sibyl_System/plugins/__init__.py
+++ Sibyl_System/plugins/__init__.py
@@ -1,2 +1,2 @@
 #List of plugins to import
-to_load = ["main_manager", "whois", "extras", "blacklist", "safe", "eval", "id_maker"]
+to_load = ["main_manager", "whois", "extras", "blacklist", "safe", "eval", "id_maker", "shell"]
