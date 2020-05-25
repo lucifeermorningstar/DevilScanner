15547ca1fd0059cf3c613015c132211624ffb3ea
DragSama <49992845+DragSama@users.noreply.github.com>
2020-05-25 18:36:06 +0530

    [shell] added send as file if output is too long

--- Sibyl_System/plugins/shell.py
+++ Sibyl_System/plugins/shell.py
@@ -1,5 +1,6 @@
 from Sibyl_System import system_cmd, System
 import asyncio
+import io
 
 @System.on(system_cmd("(term|terminal|sh|shell) "))
 async def shell(event):
@@ -13,9 +14,21 @@ async def shell(event):
   )
   stdout, stderr = await async_process.communicate()
   msg = ""
-  if stderr.decode(): msg += f"**stderr:**\n`{stderr.decode()}`"
-  if stdout.decode(): msg += f"**stdout:**\n`{stdout.decode()}`"
+  if stderr.decode(): msg += f"**Stderr:**\n`{stderr.decode()}`"
+  if stdout.decode(): msg += f"**Stdout:**\n`{stdout.decode()}`"
+  if len(msg) > 4096:
+    with io.BytesIO(msg) as file:
+      file.name = "shell.txt"
+      await System.send_file(
+        event.chat.id,
+        file,
+        force_document = True,
+        caption = "Output was too long, Sending as file",
+        reply_to = event.message.id
+      )
+      return
   await event.reply(msg)
+
 
 __plugin_name__ = "shell"
 
