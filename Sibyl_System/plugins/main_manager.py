5511a121648b3e28c4c37ebf9d7f91fa4e33629e
DragSama <49992845+DragSama@users.noreply.github.com>
2020-05-26 22:11:20 +0530

    changed proof regex and finally figured out good alternative

--- Sibyl_System/plugins/main_manager.py
+++ Sibyl_System/plugins/main_manager.py
@@ -108,7 +108,8 @@ async def approve(event):
             # checks to not gban the Gbanner and find who is who
             if reply == me.id:
                 list = re.findall(r'tg://user\?id=(\d+)', replied.text)
-                reason = re.search(r"(\*\*)?Scan Reason:(\*\*)? `?(.*)(?=`?)", replied.text).group(3)
+                reason = re.search(r"(\*\*)?Scan Reason:(\*\*)? (`([^`]*)`|.*)", replied.text)
+                reason = reason.group(4) if reason.group(4) else regex.group(3)
                 if len(list) > 1:
                     id1 = list[0]
                     id2 = list[1]
@@ -146,7 +147,8 @@ async def proof(event):
         await msg.edit('Fetching msg details from case file ID <<<<<<<')
         proof = await System.get_messages(Sibyl_logs, ids=proof_id)
         try:
-            reason = re.search(r"(\*\*)?Scan Reason:(\*\*)? `?([\w\s\d]+)", proof.message).group(3)
+            reason = re.search(r"(\*\*)?Scan Reason:(\*\*)? (`([^`]*)`|.*)", replied.text)
+            reason = reason.group(4) if reason.group(4) else regex.group(3)
         except BaseException:
             await msg.edit('>>>>Unable to see the msg or the case file ID is not valid')
             return
