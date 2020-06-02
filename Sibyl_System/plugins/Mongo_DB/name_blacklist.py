b379932076e5b0918301b15ca1e5129c1f0c3483
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-02 16:14:41 +0530

    await find_one

--- Sibyl_System/plugins/Mongo_DB/name_blacklist.py
+++ Sibyl_System/plugins/Mongo_DB/name_blacklist.py
@@ -19,7 +19,7 @@ async def update_wlc_blacklist(word, add = False):
           return False
      upd['blacklisted_wlc'] = current
      owo['$set'] = upd
-     await db.update_one(db.find_one({'_id': 2}), owo)
+     await db.update_one(await db.find_one({'_id': 2}), owo)
      return True
 
 
