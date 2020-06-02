eb955d0acb464ee16d517977dd4bd1c5326898b7
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-02 15:01:18 +0530

    put . before import name

--- Sibyl_System/__init__.py
+++ Sibyl_System/__init__.py
@@ -6,7 +6,7 @@ import os
 from motor import motor_asyncio
 import re
 import asyncio
-from client_class import SibylClient
+from .client_class import SibylClient
 
 ENV = bool(os.environ.get('ENV', False))
 if ENV:
