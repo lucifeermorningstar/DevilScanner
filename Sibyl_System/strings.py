10336539b0074ecfc1f5b49631f0fe347571b78d
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-02 10:55:40 +0530

    added forced scan string

--- Sibyl_System/strings.py
+++ Sibyl_System/strings.py
@@ -4,12 +4,20 @@ Connection successful.
 `Dominator` is now active. Cymatic scans are now possible!
 """
 
-# Make sure not to change this too much
+# Make sure not to change these too much
 # If you still wanna change it change the regex too
 scan_request_string = """
 $SCAN
 Cymatic Scan request
 **Enforcer:** {enforcer} 
+**User scanned:** {spammer}
+**Scan Reason:** `{reason}`
+**Chat Originated from:** {chat}
+**Target Message:** `{message}`
+"""
+forced_scan_string = """
+$FORCED
+**Inspector:** {ins}
 **User scanned:** {spammer}
 **Scan Reason:** `{reason}`
 **Chat Originated from:** {chat}
