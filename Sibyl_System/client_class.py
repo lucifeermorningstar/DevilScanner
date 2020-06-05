6ea2a0cdd9f4450cd2a733c1a8c94c050e2ee0ed
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-05 15:04:47 +0530

    Put enforcer id integer and not string.

--- Sibyl_System/client_class.py
+++ Sibyl_System/client_class.py
@@ -29,7 +29,7 @@ class SibylClient(TelegramClient):
             await self.send_message(Sibyl_approved_logs, bot_gban_string.format(enforcer=enforcer, scam=target, reason = reason))
         else:
             await self.send_message(Sibyl_approved_logs, scan_approved_string.format(enforcer=enforcer, scam=target, reason = reason, proof_id = msg_id))
-        if await update_gban(victim = target, reason=reason, proof_id=msg_id, enforcer=enforcer): 
+        if await update_gban(victim = int(target), reason=reason, proof_id=int(msg_id), enforcer=int(enforcer)): 
             return True
         else:
             return False
