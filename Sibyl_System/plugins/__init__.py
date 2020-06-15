7e2aafb0fc7ea2334bd98ebe67be1d255140ace0
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-15 14:23:42 +0530

    Added proofs in to_load

--- Sibyl_System/plugins/__init__.py
+++ Sibyl_System/plugins/__init__.py
@@ -1,2 +1,3 @@
 #List of plugins to import
-to_load = ["main_manager", "whois", "extras", "blacklist", "safe", "eval", "id_maker", "shell"]
+to_load = ["main_manager", "whois", "extras", "blacklist", "safe",
+           "eval", "id_maker", "shell", "proofs"]
