fa7ceade762e2dfc4025dd9b9ae1038479d9f44b
DragSama <49992845+DragSama@users.noreply.github.com>
2020-06-17 12:19:53 +0530

    Clean up client class

--- Sibyl_System/client_class.py
+++ Sibyl_System/client_class.py
@@ -1,11 +1,11 @@
 from telethon import TelegramClient
-from .strings import scan_approved_string, bot_gban_string, reject_string, proof_string, forced_scan_string
+from .strings import scan_approved_string, bot_gban_string, proof_string, forced_scan_string
 from Sibyl_System import Sibyl_logs, Sibyl_approved_logs, GBAN_MSG_LOGS, BOT_TOKEN, API_ID_KEY, API_HASH_KEY
 from Sibyl_System.plugins.Mongo_DB.gbans import update_gban
 
-class SibylClient(TelegramClient):  
-    
-    """Custom Telethon client class."""
+class SibylClient(TelegramClient):
+    """SibylClient - Subclass of Telegram Client."""
+
     def __init__(self, *args, **kwargs):
         """Declare stuff."""
         self.gban_logs = GBAN_MSG_LOGS
@@ -36,19 +36,19 @@ class SibylClient(TelegramClient):
             await self.send_message(Sibyl_approved_logs, bot_gban_string.format(enforcer=enforcer, scam=target, reason = reason))
         else:
             await self.send_message(Sibyl_approved_logs, scan_approved_string.format(enforcer=enforcer, scam=target, reason = reason, proof_id = msg_id))
-        if await update_gban(victim = int(target), reason=reason, proof_id=int(msg_id), enforcer=int(enforcer)): 
+        if await update_gban(victim = int(target), reason=reason, proof_id=int(msg_id), enforcer=int(enforcer)):
             return True
         else:
             return False
     
-    async def ungban(self, target=None, reason=None) -> bool:
+    async def ungban(self, target:int=None, reason:str=None) -> bool:
         if self.gban_logs:
             logs = self.gban_logs
         else:
             logs = self.log
-        await self.send_message(logs, f'/ungban [{target}](tg://ueser?id={target}) {reason}')
-        await self.send_message(logs, f'/unfban [{target}](tg://ueser?id={target}) {reason}')
-        if await update_gban(victim = target, reason=reason, proof_id=msg_id, enforcer=enforcer, add=False): 
+        await self.send_message(logs, f'/ungban [{target}](tg://user?id={target}) {reason}')
+        await self.send_message(logs, f'/unfban [{target}](tg://user?id={target}) {reason}')
+        if await update_gban(victim = target, add=False): 
             return True
         else:
             return False
