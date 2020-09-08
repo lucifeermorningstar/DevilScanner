9017c27b56af805adbe7262e23511e2e32a842cb
DragSama <49992845+DragSama@users.noreply.github.com>
2020-09-08 16:55:42 +0530

    fix ?

--- Sibyl_System/plugins/main_manager.py
+++ Sibyl_System/plugins/main_manager.py
@@ -42,7 +42,7 @@ async def scan(event):
               await event.reply('Invalid url')
               return
            try:
-              message = await System.get_messages(data[0], data[1])
+              message = await System.get_messages(data[0], ids = data[1])
            except:
               await event.reply('Failed to get data from url')
               return
