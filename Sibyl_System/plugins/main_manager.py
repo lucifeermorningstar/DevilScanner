26835faa414e0faae25fb8ccc5f3244a4b64b434
DragSama <49992845+DragSama@users.noreply.github.com>
2020-09-09 12:05:18 +0530

    Make id integer

--- Sibyl_System/plugins/main_manager.py
+++ Sibyl_System/plugins/main_manager.py
@@ -169,7 +169,7 @@ async def reject(event):
         _orig = re.search(r"t.me/c/(\w+)/(\d+)", replied.text)
         flags, reason = seprate_flags(event.text)
         if _orig and 'r' in flags.keys():
-          await System.send_message(_orig.group(1), 'Crime coefficient less than 100\nUser is not a target for enforcement action\nTrigger of dominator will be locked.', reply_to = int(_orig.group(2)))
+          await System.send_message(int(_orig.group(1)), 'Crime coefficient less than 100\nUser is not a target for enforcement action\nTrigger of dominator will be locked.', reply_to = int(_orig.group(2)))
         if orig and 'r' in flags.keys():
           await System.send_message(orig.group(1),'Crime coefficient less than 100\nUser is not a target for enforcement action\nTrigger of dominator will be locked.', reply_to=int(orig.group(2)))
 help_plus = """
