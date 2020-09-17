789f62fe57a95691253f68fbd4d507888a03b73a
Sawada Tsunayoshi <animekaizokux@gmail.com>
2020-09-17 19:49:16 +0530

    Minor edit to bat

--- SetupVirtualEnv_run_once.bat
+++ SetupVirtualEnv_run_once.bat
@@ -1,3 +1,4 @@
+echo off
 TITLE Setting up virtual env
 :: Running it once is fine, this just sets up virtual env >> install all modules there 
 py -m venv env && env\scripts\activate.bat && pip install -r requirements.txt
