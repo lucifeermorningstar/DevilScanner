fe78ae4f40993dda24f8649a465d5dd615afaba1
DragSama <49992845+DragSama@users.noreply.github.com>
2020-10-07 14:58:35 +0530

    Change stuff

--- Sibyl_System/plugins/Mongo_DB/tree.py
+++ Sibyl_System/plugins/Mongo_DB/tree.py
@@ -9,14 +9,14 @@ async def get_data() -> dict:
 
 async def add_inspector(sibyl: int, inspector: int) -> True:
     data = await get_data()
-    data[sibyl][inspector] = []
+    data['data'][sibyl][inspector] = []
     data['standalone'][inspector] = {'addedby': sibyl, 'timestamp': datetime.timestamp(datetime.now())}
     await db.replace_one(await get_data(), data)
 
 async def add_enforcers(inspector: int, enforcer: int) -> True:
     data = await get_data()
     sibyl = data['standalone'][inspector]['addedby']
-    data[sibyl][inspector].append([enforcer])
+    data['data'][sibyl][inspector].append([enforcer])
     data['standalone'][enforcer] = {'addedby': inspector, 'timestamp': datetime.timestamp(datetime.now())}
     await db.replace_one(await get_data(), data)
     
