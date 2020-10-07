1fdf2759e9cc566254d4bddd1ddc2d95e1529f46
DragSama <49992845+DragSama@users.noreply.github.com>
2020-10-07 14:36:25 +0530

    Make sample collection for id 4

--- Sibyl_System/__init__.py
+++ Sibyl_System/__init__.py
@@ -60,22 +60,24 @@ System = SibylClient(
 collection = MONGO_CLIENT['Sibyl']['Main']
 
 async def make_collections() -> str:
-    if await collection.count_documents({'_id': 1}, limit=1) == 0:
+    if await collection.count_documents({'_id': 1}, limit=1) == 0: # Blacklisted words list
         dictw = {"_id": 1}
         dictw["blacklisted"] = []
         await collection.insert_one(dictw)
 
-    if await collection.count_documents({'_id': 2}, limit=1) == 0:
+    if await collection.count_documents({'_id': 2}, limit=1) == 0: # Blacklisted words in name list
         dictw = {"_id": 2, "Type": "Wlc Blacklist"}
         dictw["blacklisted_wlc"] = []
         await collection.insert_one(dictw)
-    if await collection.count_documents({'_id': 3}, limit=1) == 0:
+    if await collection.count_documents({'_id': 3}, limit=1) == 0: # Gbanned users list
         dictw = {"_id": 3, "Type": "Gban:List"}
         dictw["victim"] = []
         dictw["gbanners"] = []
         dictw["reason"] = []
         dictw["proof_id"] = []
         await collection.insert_one(dictw)
+    if await collection.count_documents({'_id': 4}, limit=1) == 0: # Rank tree list
+        await collection.insert_one({'Data': {}})
     return ""
 
 def system_cmd(pattern=None, allow_sibyl=True,
