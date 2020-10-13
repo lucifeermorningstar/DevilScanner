e40c220a3c6ed7cdd4d2e4a6cfbfffe85675ae1e
DragSama <49992845+DragSama@users.noreply.github.com>
2020-10-13 12:37:36 +0530

    [__init__] Get data from elevated users file instead of config

--- Sibyl_System/__init__.py
+++ Sibyl_System/__init__.py
@@ -5,7 +5,7 @@ from telethon.sessions import StringSession
 
 from motor import motor_asyncio
 import aiohttp
-
+import json
 from datetime import datetime
 import logging
 import os
@@ -43,9 +43,11 @@ else:
     API_HASH_KEY = Config.API_HASH
     STRING_SESSION = Config.STRING_SESSION
     MONGO_DB_URL = Config.MONGO_DB_URL
-    SIBYL = Config.SIBYL
-    ENFORCERS = Config.ENFORCERS
-    INSPECTORS = Config.INSPECTORS
+    with open(os.path.join(os.getcwd(), 'Sibyl_System\\elevated_users.json'), 'r') as f:
+        data = json.load(file)
+    SIBYL = data["SIBYL"]
+    ENFORCERS = data["ENFORCERS"]
+    INSPECTORS = data["INSPECTORS"]
     Sibyl_logs = Config.Sibyl_logs
     Sibyl_approved_logs = Config.Sibyl_approved_logs
     GBAN_MSG_LOGS = Config.GBAN_MSG_LOGS
