18fd9ad4810145265f659610bf10451a45a57e5b
DragSama <49992845+DragSama@users.noreply.github.com>
2020-11-25 12:42:06 +0530

    Testing proof message builder and change indent to 4 from 2

--- Sibyl_System/plugins/bot.py
+++ Sibyl_System/plugins/bot.py
@@ -1,23 +1,29 @@
-from Sibyl_System import System, session
-from telethon import events
-from Sibyl_System import INSPECTORS, Sibyl_logs
+from Sibyl_System import System, session, INSPECTORS, Sibyl_logs
+from Sibyl_System.strings import proof_string, scan_request_string
 from Sibyl_System.plugins.Mongo_DB.gbans import get_gban
-from Sibyl_System.strings import proof_string
-import asyncio
+
+from telethon.custom import Button
+from telethon import events
+
 import re
+import asyncio
+
+data = []
+DATA_LOCK = asyncio.Lock()
+
 
 async def make_proof(user):
-        data = await get_gban(user)
-        if not data:
-            return False
-        message = data.get('message') or ''
-        async with session.post('https://nekobin.com/api/documents', json={'content': message}) as r:
-            paste = f"https://nekobin.com/{(await r.json())['result']['key']}"
-        url = "https://del.dog/documents"
-        async with session.post(url, data=message.encode("UTF-8")) as f:
-             r = await f.json()
-             url = f"https://del.dog/{r['key']}"
-        return proof_string.format(proof_id = data['proof_id'], reason=data['reason'], paste=paste, url=url)
+    data = await get_gban(user)
+    if not data:
+        return False
+    message = data.get('message') or ''
+    async with session.post('https://nekobin.com/api/documents', json={'content': message}) as r:
+        paste = f"https://nekobin.com/{(await r.json())['result']['key']}"
+    url = "https://del.dog/documents"
+    async with session.post(url, data=message.encode("UTF-8")) as f:
+         r = await f.json()
+         url = f"https://del.dog/{r['key']}"
+    return proof_string.format(proof_id = data['proof_id'], reason=data['reason'], paste=paste, url=url)
 
 @System.bot.on(events.NewMessage(pattern = "[/?]start"))
 async def sup(event):
@@ -30,18 +36,18 @@ This bot is a inline bot, You can use it by typing `@SibylSystemRobot`
 If a user is gbanned -
     Getting reason for gban, message the user was gbanned for - `@SibylSystemRobot proof <user_id>`
     """)
-        
+
 
 @System.bot.on(events.InlineQuery)  # pylint:disable=E0602
 async def inline_handler(event):
-  builder = event.builder
-  query = event.text
-  split = query.split(' ', 1)
-  if event.query.user_id not in INSPECTORS:
+    builder = event.builder
+    query = event.text
+    split = query.split(' ', 1)
+    if event.query.user_id not in INSPECTORS:
     result = builder.article("Sibyl System", text = "You don't have access to this cmd.")
     await event.answer([result])
     return
-  if query.startswith("proof"):
+    if query.startswith("proof"):
       if len(split) == 1:
          result = builder.article("Type Case-ID", text="No Case-ID was provided")
       else:
@@ -51,6 +57,25 @@ async def inline_handler(event):
                                      text="Unknown error occured while getting proof from Case-ID")
          else:
             result = builder.article("Proof", text = proof, link_preview=False)
-  else:
-    result = builder.article("No type provided", text="Use proof <user_id> to get proof")
-  await event.answer([result])
+    elif query.startswith("builder"):
+      split = query.split(':::', 4)
+      print(split)
+      if len(split) != 5:
+          result = builder.article('Not enough info provided...')
+      else:
+          u_id, enforcer, source, reason, message = split
+          dict_ = {'u_id': u_id, "enforcer": enforcer, "source": source, "reason": reason, "message": message}
+          print(dict_)
+          async with DATA_LOCK:
+              data.append(dict_)
+              index = data.index(dict_)
+          buttons = [Button.inline("Approve", data = f"approve_{data}"), Button.inline("Reject", data = f"reject_{data}")]
+          result = builder.article(
+                "Output",
+                text = scan_request_string.format(enforcer = enforcer, spammer = u_id, reason = reason, source = source, message = message),
+                buttons = buttons
+          )
+
+    else:
+    result = builder.article("No type provided", text="Use\nproof <user_id> to get proof\nbuilder id:::enforcer:::source:::reason:::message")
+    await event.answer([result])
