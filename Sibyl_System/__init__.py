41abaee8a17ec1767e4f524fac80b9bfa1fd6332
DragSama <49992845+DragSama@users.noreply.github.com>
2021-01-09 12:08:04 +0530

    fix deleting log file even if it doesn't exist

--- Sibyl_System/__init__.py
+++ Sibyl_System/__init__.py
@@ -11,8 +11,8 @@ import logging
 import os
 import re
 
-
-os.remove("log.txt")
+if os.path.exists('log.txt')
+    os.remove("log.txt")
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
